beb0bd49602739dd11489e37bfedc904b5ec22cf
Emycoin <emytausevich@gmail.com>
2021-07-22 08:46:44 -0400

    completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,22 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function displayBio(bio) {
+  if(bio) {
+    return <p>{bio}</p>
+  }
+  return null;
+}
+
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {displayBio(props.bio)}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links github={props.links.github} linkedin={props.links.linkedin} />
+
     </div>
   );
 }
